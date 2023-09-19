c9e09e69f8385d048f53fcbc10500bf13b0ec65f
hunkim00 <kimhun1027@naver.com>
2023-09-20 01:31:05 +0900

    <feat> spring start setting

---
+++ src/main/java/com/ivekorea/ivekorea_be/IveKoreaBeApplication.java
@@ -0,0 +1,13 @@
+package com.ivekorea.ivekorea_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IveKoreaBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IveKoreaBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ivekorea/ivekorea_be/IveKoreaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivekorea.ivekorea_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IveKoreaBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
